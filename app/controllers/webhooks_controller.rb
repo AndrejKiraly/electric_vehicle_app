40f7164c3fd23a3425b210b8c30f4a5f3399061a
AndrejKiraly <kiralyandrej24@gmail.com>
2024-05-09 17:41:29 +0200

    Refactor webhook routes for better organization and clarity

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,19 @@
+class WebhooksController < ApplicationController
+
+    def index 
+        render json: { message: 'Webhook index' }
+    end
+
+    def create
+        # Handle the webhook POST request here
+        # Access the request parameters using params[:param_name]
+        # Process the webhook data and perform necessary actions
+
+        # Example: Log the webhook data
+        Rails.logger.info("Webhook received: #{params}")
+
+
+        # Example: Send a response back to the webhook sender
+        render json: { message: 'Webhook received successfully',data:  }, status: :ok
+    end
+end
