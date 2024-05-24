3f70e95908944f64fd2aef1109f0d90b8d2b2aa3
AndrejKiraly <kiralyandrej24@gmail.com>
2024-05-24 17:10:49 +0200

    charging creator fix

--- app/services/charging_creator.rb
+++ app/services/charging_creator.rb
@@ -7,8 +7,8 @@ class ChargingCreator
     def create
       set_lat_lng
       
-      Charging.new(@params)
-      Charging.user_id = User.where(uid: request.headers["uid"]).first.id
+      charging = Charging.new(@params)
+      charging.user_id = User.where(uid: request.headers["uid"]).first.id
 
     end
   
